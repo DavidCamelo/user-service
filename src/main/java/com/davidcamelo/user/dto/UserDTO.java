09fd3cf8f5981a56e1faa371d88896617142d4cd
David Camelo <dacaro16@gmail.com>
2025-05-05 15:36:53 -0500

    Setup User Service

--- src/main/java/com/davidcamelo/user/dto/UserDTO.java
+++ src/main/java/com/davidcamelo/user/dto/UserDTO.java
@@ -1,12 +1,14 @@
 package com.davidcamelo.user.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record UserDTO (
-    Long id,
-    String name,
-    String lastName
+        @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+        Long id,
+        String name,
+        String lastName
 ) { }
