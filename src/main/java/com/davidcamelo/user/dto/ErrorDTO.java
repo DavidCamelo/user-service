7f6e5a3770741dcfae5e536839bb6fa8aeff253b
David Camelo <dacaro16@gmail.com>
2025-05-06 14:25:29 -0500

    Setup User Service

--- src/main/java/com/davidcamelo/user/dto/ErrorDTO.java
+++ src/main/java/com/davidcamelo/user/dto/ErrorDTO.java
@@ -1,16 +1,13 @@
 package com.davidcamelo.user.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 
 import java.util.Date;
 
 @Builder
 public record ErrorDTO (
-        @JsonProperty(access = JsonProperty.Access.READ_ONLY)
         String message,
-        @JsonProperty(access = JsonProperty.Access.READ_ONLY)
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
         Date timestamp
 ) { }
