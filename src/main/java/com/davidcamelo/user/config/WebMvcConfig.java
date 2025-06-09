31bb36168102710d8b73b43be5a1110e4e477add
David Camelo <dacaro16@gmail.com>
2025-06-09 15:49:21 -0500

    Setup Product Service

--- src/main/java/com/davidcamelo/user/config/WebMvcConfig.java
+++ src/main/java/com/davidcamelo/user/config/WebMvcConfig.java
@@ -10,6 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOrigins("https://react-ui.davidcamelo.com")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowedOriginPatterns("*")
