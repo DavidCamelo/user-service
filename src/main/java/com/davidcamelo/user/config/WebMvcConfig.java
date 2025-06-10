3fcf03f4b30858016791c598cd1822a0fb88cf94
David Camelo <dacaro16@gmail.com>
2025-06-09 20:56:53 -0500

    Setup User Service

--- src/main/java/com/davidcamelo/user/config/WebMvcConfig.java
+++ src/main/java/com/davidcamelo/user/config/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://react-ui.davidcamelo.com")
+                .allowedOrigins("https://components-ui.davidcamelo.com", "https://user-ui.davidcamelo.com", "https://product-ui.davidcamelo.com", "https://react-ui.davidcamelo.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS", "TRACE", "CONNECT")
                 .allowedHeaders("Content-Type", "Authorization")
                 .allowCredentials(true);
