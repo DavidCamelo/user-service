088956677e497b5adc6f9db14d1fae16a3a01c50
David Camelo <dacaro16@gmail.com>
2025-12-03 15:26:09 -0500

    Upgrade to Spring Boot 4

---
+++ src/main/java/com/davidcamelo/user/UserServiceApplication.java
@@ -0,0 +1,25 @@
+package com.davidcamelo.user;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+@OpenAPIDefinition(
+        servers = { @Server(url = "/users", description = "User Service URL"), @Server(url = "/", description = "Default Server") },
+        info = @Info(title = "OpenAPI definition", version = "v0"))
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        log.info("Current java.home {}", System.getProperty("java.home"));
+        log.info("Current java.vendor {}", System.getProperty("java.vendor"));
+        log.info("Current java.vendor.url {}", System.getProperty("java.vendor.url"));
+        log.info("Current java.version {}", System.getProperty("java.version"));
+
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+}
